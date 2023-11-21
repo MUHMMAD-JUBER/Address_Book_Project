fe66b57cb6b0f786a2ba954b1d7d2da5743a3bfd
Zubair <zubairyaqoob6988@gmail.com>
2023-11-21 15:25:48 +0530

    Added: ability to create contact

---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+public class Contact {
+    String firstname;
+    String lastname;
+    String address;
+    String city;
+    String state;
+    int zip;
+    int phone;
+    String email;
+}
