6ea5e65f0f18af35095db087b49c358a791e44dd
Zubair <zubairyaqoob6988@gmail.com>
2023-11-22 14:34:38 +0530

    Added: conatcs like name adress and all

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -3,5 +3,6 @@ package com.bridgelabz;
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("WELCOME TO ADDRESS BOOK PROGRAM");
+        System.out.println();
     }
 }
--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -1,8 +1,5 @@
 package com.bridgelabz;
 public class Contact {
-    public static void main(String[] args) {
-
-
         String firstname;
         String lastname;
         String address;
@@ -11,5 +8,81 @@ public class Contact {
         int zip;
         int phone;
         String email;
+
+    //Initializing contact values
+    public Contact(String firstname, String lastname, String address, String city, String state, int zip, int phone, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
